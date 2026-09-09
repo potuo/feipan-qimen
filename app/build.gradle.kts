260bf4277e47d921dac01f895c199c5b728fdf81
potuo <potuo233@hotmail.com>
2026-09-09 22:46:02 +0800

    release: v5.0.0 bump + 三件套更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.potuo.feipanqimen2"
         minSdk = 26
         targetSdk = 34
-        versionCode = 29
-        versionName = "4.5.2"
+        versionCode = 30
+        versionName = "5.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
